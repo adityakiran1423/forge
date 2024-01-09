54be4add7b060495bf6ac449e3444dd9c9ee4e38
adityakiran1423 <adi.kiran@gmail.com>
2024-01-09 10:01:28 +0530

    add create logic | adding comments to languages.py

--- app/forge.py
+++ app/forge.py
@@ -4,28 +4,54 @@ import typer
 from rich import print as rprint
 import sqlite3
 
-rprint("[sandy_brown]Welcome to Forge[/sandy_brown]")
+print()
 
-app=typer.Typer()
+rprint("[sandy_brown]+------------------+[/sandy_brown]")
+rprint("[sandy_brown]| Welcome to Forge |[/sandy_brown]")
+rprint("[sandy_brown]+------------------+[/sandy_brown]", end='\n\n')
 
-"use if and else to count number of args passed when running command, if 2 print welcome"
-"if more than it will redirect to app.command section"
+app = typer.Typer()
 
 @app.command()
 def create():
-    rprint("[gold1]implement logic for creating template here[/gold1]")
+    rprint("[gold3]Implementing logic for creating template here[/gold3]")
+    name=input("Please enter the name of the project  :  ")
+    rprint(f"[gold3]The name of the project you have entered is {name.replace(" ","")}[/gold3]")
+    db_creation_choice=input("Please enter 'y' to proceed with creation of Database, enter 'n' to abort  :  ")
+    if db_creation_choice.lower()=='y':
+        rprint(f"[gold3]The name of the database is {name.replace(" ","")}[/gold3]")
+        rprint("[gold3]Please enter 'y' to proceed.[/gold3]")
+        rprint("[gold3]Enter 'n' to change the name of the databse.[/gold3]")
+        database_name_change=input()
+        if database_name_change=='n':
+            rprint("[gold3]Please enter the name of the database according to your need.[/gold3]")
+            user_edited_database_name=input()
+            rprint(f"[green1]Database name changed to {user_edited_database_name.replace(" ","")}[/green1]")
+
+        " ------------------------------------ "
+        " implement creation of database logic "
+        " ------------------------------------ "
+
+    elif db_creation_choice.lower()=='n':
+        rprint("[deep_pink2]Database name not changed[/deep_pink2]")
+        exit(1)
+    else:
+        exit(1)
 
 @app.command()
 def status():
-    rprint("[gold1]implement logic for printing all info about project here[/gold1]")
+    rprint("[gold3]implement logic for printing all info about project here[/gold3]")
+
 
 @app.command()
 def update():
-    rprint("[gold1]implement logic for updating info about project here[/gold1]")
+    rprint("[gold3]implement logic for updating info about project here[/gold3]")
+
 
 @app.command()
 def show():
-    rprint("[gold1]implement logic for printing specified paramenter of project here[/gold1]")
+    rprint("[gold3]implement logic for printing specified paramenter of project here[/gold3]")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app()
--- app/languages.py
+++ app/languages.py
@@ -12,25 +12,26 @@ language_items = soup.find_all("li")  # Target all <li> tags
 # Extract language names
 programming_languages = [item.text.strip() for item in language_items]  # Remove whitespaces
 
+# Extract relevant languages
 programming_languages = programming_languages[124:814]
 
 # for language in programming_languages:
 #     print(language)
 
-temp_lang_list=[]
-languages_dict={}
+temp_lang_list = []
+languages_dict = {}
 
 for language in range(len(programming_languages)):
-    if programming_languages[language][1:]==programming_languages[language+1][1:]:
+    if programming_languages[language][1:] == programming_languages[language + 1][1:]:
         temp_lang_list.append(programming_languages[language])
     else:
-        " implement line 31 logic "
+        "implement line 31 logic"
     pass
 
-'''
+"""
 language_dict={
     'A': [''],
     'B': ['B','Babbage','Ballerina','Bash','BASIC','Batch file','bc','BCPL','BeanShell','BETA','BLISS','Blocky','BlooP','Boo','Boomerang','Bosque'],
     'C': []
 }
-'''
+"""
