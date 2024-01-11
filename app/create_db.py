59fa0fba82b3246c5d8398588b7e2e2566ec1314
adityakiran1423 <adi.kiran@gmail.com>
2024-01-11 12:28:48 +0530

    adding db creation file

---
+++ app/create_db.py
@@ -0,0 +1,21 @@
+'''
+Make a new file for creating a database and then import it
+
+implement dataBase creation here
+Creation of only one dataBase, create three tables :
+1. Name Table (stores the name of the projects)
+    * name (primary key)
+2. Description Table (stores details about the project)
+    * name (foreign key)
+    * description
+    * aim
+    * resources
+    * status
+    * github link
+    * tech stack (primary key)
+3. Tech Table (stores the tech stack of the project)
+    * frontend
+    * backend
+    * database
+    * web frame work 
+'''
