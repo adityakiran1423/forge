7af7a8308d1e877dbf70ead4b08aa4ec8f4daf6d
adityakiran1423 <adi.kiran@gmail.com>
2024-02-24 22:14:19 +0530

    added table creation

--- setup.py
+++ setup.py
@@ -19,3 +19,26 @@ else:
     print("Connected to existing database.")
 
 c = conn.cursor()
+
+create_names='''create table if not exists names(
+                project_name primary key TEXT, 
+                project_id TEXT, 
+                time_stamp TIMESTAMP
+                )'''
+c.execute(create_names)
+
+create_descriptions='''create table if not exists descriptions(
+                        project_name TEXT,
+                        desc TEXT, 
+                        status TEXT, 
+                        aim TEXT
+                        FOREIGN KEY (project_name) REFERENCES names(project_name)
+                    )'''
+c.execute(create_descriptions)
+
+create_resources='''create table if not exists resources(
+                    resources TEXT,
+                    project_name TEXT,
+                    FOREIGN KEY (project_name) REFERENCES name(project_name) 
+                    )'''
+c.execute(create_resources)
