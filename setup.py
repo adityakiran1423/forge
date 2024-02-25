33aade547b22c7fc09424e5dee810303ad104ab5
adityakiran1423 <adi.kiran@gmail.com>
2024-02-25 09:06:41 +0530

    changed names to have date and time seperate instead of datetime

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ import os
 'creates the db once here'
 'include requirements.txt and ensure set up is complete when this file is run once'
 'all dependencies should be installed and Forge should be ready to go'
+'ensures that forge can be run from anywhere in the terminal'
 
 database_path="database.db"
 
 if not os.path.exists(database_path):
     # Create the database if it doesn't exist
-    conn = sqlite3.connect(database_path,
-                           detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    # conn = sqlite3.connect(database_path,
+    #                        detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    conn = sqlite3.connect(database_path)
     print("Database created successfully!")
 else:
     # Connect to the existing database
@@ -22,10 +24,13 @@ c = conn.cursor()
 
 create_names='''create table if not exists names(
                 project_name primary key TEXT, 
-                project_id TEXT, 
-                time_stamp TIMESTAMP
+                project_id TEXT,
+                date TEXT,
+                time TEXT 
                 )'''
+
 c.execute(create_names)
+
 
 create_descriptions='''create table if not exists descriptions(
                         project_name TEXT,
@@ -34,11 +39,14 @@ create_descriptions='''create table if not exists descriptions(
                         aim TEXT
                         FOREIGN KEY (project_name) REFERENCES names(project_name)
                     )'''
+
 c.execute(create_descriptions)
+
 
 create_resources='''create table if not exists resources(
                     resources TEXT,
                     project_name TEXT,
                     FOREIGN KEY (project_name) REFERENCES name(project_name) 
                     )'''
+
 c.execute(create_resources)
