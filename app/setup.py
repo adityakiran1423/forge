389792cad2b7df1ac3b96c1db33df256204e9416
adityakiran1423 <adi.kiran@gmail.com>
2024-05-03 02:27:45 +0530

    commented connected to db message

--- app/setup.py
+++ app/setup.py
@@ -2,13 +2,13 @@ import sqlite3
 import os
 
 
-'creates the db once here'
-'include requirements.txt and ensure set up is complete when this file is run once'
-'all dependencies should be installed and Forge should be ready to go'
-'ensures that forge can be run from anywhere in the terminal'
+"creates the db once here"
+"include requirements.txt and ensure set up is complete when this file is run once"
+"all dependencies should be installed and Forge should be ready to go"
+"ensures that forge can be run from anywhere in the terminal"
 
 
-database_path="database.db"
+database_path = "database.db"
 
 if not os.path.exists(database_path):
     # Create the database if it doesn't exist
@@ -19,37 +19,37 @@ if not os.path.exists(database_path):
 else:
     # Connect to the existing database
     conn = sqlite3.connect(database_path)
-    print("Connected to existing database.")
+    # print("Connected to existing database.")
 
 # conn=sqlite3.connect(database_path)
 c = conn.cursor()
 
-create_names='''create table if not exists names(
+create_names = """create table if not exists names(
                 project_name TEXT primary key, 
                 project_id TEXT,
                 date TEXT,
                 time TEXT 
-                )'''
+                )"""
 
 c.execute(create_names)
 
 
-create_descriptions='''create table if not exists descriptions(
+create_descriptions = """create table if not exists descriptions(
                         project_name TEXT,
                         desc TEXT, 
                         status TEXT, 
                         aim TEXT,
                         FOREIGN KEY (project_name) REFERENCES names(project_name)
-                    )'''
+                    )"""
 
 c.execute(create_descriptions)
 
 
-create_resources='''create table if not exists resources(
+create_resources = """create table if not exists resources(
                     resources TEXT,
                     project_name TEXT,
                     FOREIGN KEY (project_name) REFERENCES name(project_name) 
-                    )'''
+                    )"""
 
 c.execute(create_resources)
 
