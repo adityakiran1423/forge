b787684a935a6b48ac92b74f914b8da0280f7480
adityakiran1423 <adi.kiran@gmail.com>
2024-05-25 19:50:14 +0530

    commit before gitignore

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import sqlite3
 import os
 
 
-'creates the db once here'
-'include requirements.txt and ensure set up is complete when this file is run once'
-'all dependencies should be installed and Forge should be ready to go'
-'ensures that forge can be run from anywhere in the terminal'
+"creates the db once here"
+"include requirements.txt and ensure set up is complete when this file is run once"
+"all dependencies should be installed and Forge should be ready to go"
+"ensures that forge can be run from anywhere in the terminal"
 
-database_path="database.db"
+
+database_path = "database.db"
 
 if not os.path.exists(database_path):
     # Create the database if it doesn't exist
@@ -18,38 +19,38 @@ if not os.path.exists(database_path):
 else:
     # Connect to the existing database
     conn = sqlite3.connect(database_path)
-    print("Connected to existing database.")
+    # print("Connected to existing database.")
 
+# conn=sqlite3.connect(database_path)
 c = conn.cursor()
 
-create_names='''create table if not exists names(
-                project_name primary key TEXT, 
+create_names = """create table if not exists names(
+                project_name TEXT primary key, 
                 project_id TEXT,
                 date TEXT,
                 time TEXT 
-                )'''
+                )"""
 
 c.execute(create_names)
 
 
-create_descriptions='''create table if not exists descriptions(
-                        project_name TEXT,
+create_descriptions = """create table if not exists descriptions(
+                        project_id TEXT,
                         desc TEXT, 
                         status TEXT, 
-                        aim TEXT
-                        FOREIGN KEY (project_name) REFERENCES names(project_name)
-                    )'''
+                        aim TEXT,
+                        FOREIGN KEY (project_id) REFERENCES names(project_id)
+                    )"""
 
 c.execute(create_descriptions)
 
 
-create_resources='''create table if not exists resources(
+create_resources = """create table if not exists resources(
                     resources TEXT,
-                    project_name TEXT,
-                    FOREIGN KEY (project_name) REFERENCES name(project_name) 
-                    )'''
+                    project_id TEXT,
+                    FOREIGN KEY (project_id) REFERENCES name(project_id) 
+                    )"""
 
 c.execute(create_resources)
 
-c.commit()
-c.close()
+conn.commit()
