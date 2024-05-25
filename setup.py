81a0355d0852703c357fac82f0f59489fdb6dc74
adityakiran1423 <adi.kiran@gmail.com>
2024-05-26 00:44:55 +0530

    refactor

--- setup.py
+++ setup.py
@@ -2,28 +2,17 @@ import sqlite3
 import os
 
 
-"creates the db once here"
-"include requirements.txt and ensure set up is complete when this file is run once"
-"all dependencies should be installed and Forge should be ready to go"
-"ensures that forge can be run from anywhere in the terminal"
-
-
 database_path = "database.db"
 db_creation_status = 0
 
 if not os.path.exists(database_path):
-    # Create the database if it doesn't exist
-    # conn = sqlite3.connect(database_path,
-    #                        detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
     conn = sqlite3.connect(database_path)
     db_creation_status = 1
     # print("Database created successfully!")
 else:
-    # Connect to the existing database
     conn = sqlite3.connect(database_path)
     # print("Connected to existing database.")
 
-# conn=sqlite3.connect(database_path)
 c = conn.cursor()
 
 create_names = """create table if not exists names(
